525f60ac72cd5460471b58b1399a2307f9f338b3
Paul Jonathan Zoulin <pj@x-nerds.com>
2023-01-09 19:16:41 +0000

    Make bootstrap executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotnet nuget add source https://nuget.pkg.github.com/xnerditos/index.json -n "xnerditos" --username toochevere  --store-password-in-clear-text --password $PACKAGE_ACCESS
+dotnet nuget add source https://nuget.pkg.github.com/xnerditos/index.json -n "xnerditos" --username $GITHUB_USER --store-password-in-clear-text --password $PACKAGE_ACCESS
 
 #echo 'source $HOME/.xnerds_profile' >> $HOME/.bashrc
 #source $HOME/.bashrc
